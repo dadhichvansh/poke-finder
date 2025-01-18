e5120b68c4f30e17aa29e62142dce68928d7f6b6
Vansh Dadhich <dadhichvansh46@gmail.com>
2025-01-18 23:54:20 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,20 @@
 "use strict";
 
+const pokemonImage = document.getElementsByClassName("pokemon-image");
+const pokemonName = document.getElementsByClassName("pokemon-name");
+const pokemonType = document.getElementsByClassName("pokemon-type");
+const pokemonHeight = document.getElementsByClassName("pokemon-height");
+const pokemonWeight = document.getElementsByClassName("pokemon-weight");
+const pokemonSpeed = document.getElementsByClassName("pokemon-speed");
+const pokemonAttack = document.getElementsByClassName("pokemon-attack");
+const pokemonAbility = document.getElementsByClassName("pokemon-ability");
+
 const api = "https://pokeapi.co/api/v2/pokemon?limit=50";
+
+const getPokemonData = async (api) => {
+  const response = await fetch(api);
+  const data = await response.json();
+  console.log(data);
+};
+
+getPokemonData(api);
