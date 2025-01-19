130ae67874dd14507a46049586ec99f08692fd80
Vansh Dadhich <dadhichvansh46@gmail.com>
2025-01-19 21:41:17 +0530

    Update FetchPokemon.js

--- js/FetchPokemon.js
+++ js/FetchPokemon.js
@@ -1,5 +1,3 @@
-import { displayPokemon } from "./DisplayPokemon.js";
-
 // fetch pokemon data from API
 export const fetchPokemon = async (API_URL) => {
   try {
@@ -10,12 +8,21 @@ export const fetchPokemon = async (API_URL) => {
       const response = await fetch(pokemon.url);
       const data = await response.json();
 
-      return data;
+      return {
+        name: data.name,
+        type: data.types[0].type.name,
+        height: data.height,
+        weight: data.weight,
+        speed: data.stats[0].base_stat,
+        experience: data.base_experience,
+        attack: data.stats[1].base_stat,
+        ability: data.abilities[0].ability.name,
+        image: data.sprites.other.dream_world.front_default,
+      };
     });
 
-    const allPokemonData = await Promise.all(pokemonData);
-    displayPokemon(allPokemonData);
+    return await Promise.all(pokemonData); // return resolved pokemon data
   } catch (err) {
-    console.log(err);
+    console.error("Error fetching Pokémon data:", err);
   }
 };
